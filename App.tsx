2da2986eca75c724b2ed3c3b29901673a92751d7
nikhilmakwanatatvasoft.com <nikhil.makwana@tatvasoft.com>
2023-05-02 10:20:44 +0530

    Changes

--- App.tsx
+++ App.tsx
@@ -19,14 +19,6 @@ const App = () => {
   const acceleroValue = useSharedValue({x: 0, y: 0});
   const progress = useSharedValue(-1);
   const prev = useSharedValue({x: 0, y: 0});
-  const prev2 = useSharedValue({x: 0, y: 0});
-
-  // const [points, setPoints] = useState(0);
-  // const [pointLocation, setPointLocation] = useState({top: 10, left: 10});
-
-  // const points = useRef(0);
-  // const pointLocation = useRef({top: 10, left: 10});
-
   setInterval(() => {
     if (progress.value < 0) {
       progress.value = withTiming(1, {duration: 2000});
@@ -45,28 +37,6 @@ const App = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [acceleroValue.value]);
 
-  const motionValue2 = useDerivedValue(() => {
-    const MAX_X = 250;
-    const MAX_Y = 250;
-    const MIN_X = 0;
-    const MIN_Y = 0;
-
-    let newX = prev2.value.x + acceleroValue.value.x * -1;
-    let newY = prev2.value.y + acceleroValue.value.y * 1;
-    // Can be more cleaner
-    if (Math.abs(newX) >= MAX_X || newX <= MIN_X) {
-      newX = prev2.value.x;
-    }
-    if (Math.abs(newY) >= MAX_Y || newY <= MIN_Y) {
-      newY = prev2.value.y;
-    }
-    prev2.value = {
-      x: newX,
-      y: newY,
-    };
-    return {x: newX, y: newY};
-  }, [acceleroValue.value]);
-
   const motionValue = useDerivedValue(() => {
     const MAX_X = 250;
     const MAX_Y = 250;
@@ -82,31 +52,6 @@ const App = () => {
     if (Math.abs(newY) >= MAX_Y || newY <= MIN_Y) {
       newY = prev.value.y;
     }
-    // point system
-    // console.log({
-    //   newX: Math.floor(newX),
-    //   newY: Math.floor(newY),
-    //   down: pointLocation.current.left,
-    //   up: pointLocation.current.left + 10,
-    // });
-
-    // if (
-    //   newX >= pointLocation.current.top - 10 &&
-    //   newX <= pointLocation.current.top + 10 &&
-    //   newY >= pointLocation.current.left - 10 &&
-    //   newY <= pointLocation.current.left + 10
-    // ) {
-    //   console.log({
-    //     points: points.value,
-    //   });
-    //   points.value = points.value + 1;
-    //   const randomX = Math.floor(Math.random() * 260 - 10);
-    //   const randomY = Math.floor(Math.random() * 260 - 10);
-    //   pointLocation.current = {
-    //     top: 249,
-    //     left: 249,
-    //   };
-    // }
     prev.value = {
       x: newX,
       y: newY,
@@ -137,25 +82,11 @@ const App = () => {
       ],
     };
   });
-  const mStyles = useAnimatedStyle(() => {
-    return {
-      transform: [
-        {
-          translateX: motionValue2.value.x,
-        },
-        {
-          translateY: motionValue2.value.y,
-        },
-      ],
-    };
-  });
 
   return (
     <SafeAreaView style={styles.background}>
       <Animated.View style={styles.box}>
         <Animated.View style={[styles.ball, bgColor, mStyle]} />
-        <Animated.View style={[styles.ball, bgColor, mStyles]} />
-        {/* <View style={[styles.point, pointLocation.current]} /> */}
       </Animated.View>
     </SafeAreaView>
   );
